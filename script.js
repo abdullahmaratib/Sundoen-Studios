43bc418374d296fdf5f86a47edddba0dcd4e3868
Abdullah Maratib <abdullahmaratib78@gmail.com>
2024-12-26 19:02:40 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+
+
+gsap.to("#loader",{
+
+    y: "-100%",
+    delay: 3.2
+})
+
+
+
+
+
+function swiper(){
+
+
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: "auto",
+        centeredSlides: true,
+        spaceBetween: 100,
+    
+       
+      });
+
+      
+    }
+
+swiper();
+
+
+
+
+
+ 
+
+
+
+
+
+
+    
